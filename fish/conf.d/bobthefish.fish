e0f02d360719a5235cc6b8f626c07c8d7ac35c96
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-09 13:43:42 -0300

    Fish files

---
+++ fish/conf.d/bobthefish.fish
@@ -0,0 +1,7 @@
+set -g theme_nerd_fonts yes
+set -g theme_color_scheme gruvbox
+set -g theme_dispay_user ssh
+set -g default_user gabrielcoelho
+set -g theme_display_vi yes
+set -g theme_display_git_default_branch yes
+set -g theme_use_abbreviated_branch_name yes
