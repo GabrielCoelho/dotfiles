88cb8fc2f735758efb9bd6e215a8091507dfae0b
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-30 19:47:18 -0300

    fix(noice lsp): Hover disabled

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -115,12 +115,25 @@ return {
         },
       },
       presets = { lsp_doc_border = true },
+      lsp = {
+ -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            -- override the default lsp markdown formatter with Noice
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            -- override the lsp markdown formatter with Noice
+            ["vim.lsp.util.stylize_markdown"] = true,
+            -- override cmp documentation with Noice (needs the other options to work)
+            ["cmp.entry.get_documentation"] = true,
+          },
+          hover = { enabled = false }, -- <-- HERE!
+          signature = { enabled = false }, -- <-- HERE!
+      },
     },
     dependencies = {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify",
     },
-  },
+   },
   {
     "hrsh7th/cmp-cmdline",
   },
