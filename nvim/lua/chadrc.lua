0209c4108102fc82f03d87ff52c37821b8d9618b
GabrielCoelho <gbcoe@outlook.com.br>
2024-07-02 11:51:53 -0300

    Configuring the statusline and others

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,36 +1,36 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "gruvbox",
-    statusline = {
-      theme = "default",
-      separator_style = "round", 
-      order = nil,
-      modules = nil,
-    },
-  icons = {
-      ft = "",
-      lazy = "󰂠 ",
-      loaded = "",
-      not_loaded = "",
+  theme = "gruvbox",
+  statusline = {
+    theme = "minimal",
+    separator_style = "round",
+    order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "cursor", "lsp" },
+    modules = nil,
   },
-  nvdash = {
-    load_on_startup = true,
-    header = {
-"          (\\`/\\",
-"    `=\\/\\",
-"     `=\\/\\",
-"      `=\\/",
-"         \\"  },
-  }
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  icons = {
+    ft = "",
+    lazy = "",
+    loaded = "",
+    not_loaded = "",
+    cwd = "",
+  },
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "flat_dark",
+  },
+  telescope = {
+    style = "bordered",
+  },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
 }
 
 return M
