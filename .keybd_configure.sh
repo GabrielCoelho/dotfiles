8f57c9142d8eda002bf227e74fe21fabeae7762c
GabrielCoelho <gbcoe@outlook.com.br>
2025-02-01 17:50:39 -0300

    feat(new config): Floating, Workspaces and Tmux

--- .keybd_configure.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-setxkbmap motiondvorak
-xrandr --newmode "1536x864_60.00"  109.25  1536 1624 1784 2032  864 867 872 897 -hsync +vsync
-xrandr --addmode eDP-1 "1536x864_60.00"
-xrandr --output eDP-1 --mode "1536x864_60.00"
-espanso start --unmanaged
-tmux new -d -s main -n second-brain -c ~/0.\ vaults/my_brain/
-tmux neww -n configs
-tmux neww -n dev
