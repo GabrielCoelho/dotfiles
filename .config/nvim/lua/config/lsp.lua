bc61d456e51c0bfb9f84928f2c6dd06d633b5f42
GabrielCoelho <gbcoe@outlook.com.br>
2025-03-21 12:06:16 -0300

    feat(java-jdtls): Made Lombok and Spring work <3

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -21,10 +21,12 @@ local servers = {
 
 -- Configuração padrão para servidores LSP
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = on_attach,
-		capabilities = capabilities,
-	})
+	if lsp ~= "jdtls" then
+		lspconfig[lsp].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+		})
+	end
 end
 
 -- Configurações específicas para alguns servidores LSP
@@ -67,93 +69,4 @@ lspconfig.rust_analyzer.setup({
 lspconfig.clangd.setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
-})
-
--- Java LSP
-lspconfig.jdtls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	cmd = {
-		"java",
-		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
-		"-Dosgi.bundles.defaultStartLevel=4",
-		"-Declipse.product=org.eclipse.jdt.ls.core.product",
-		"-Dlog.protocol=true",
-		"-Dlog.level=ALL",
-		"-Xmx1g",
-
-		-- Configuração do Lombok
-		"-javaagent:" .. vim.fn.expand("~/.local/share/nvim/mason/share/jdtls/lombok.jar"),
-
-		-- Suprimir avisos de incubadora
-		"-Djdt.ls.warn.incubator=false",
-
-		"--add-modules=ALL-SYSTEM",
-		"--add-opens",
-		"java.base/java.util=ALL-UNNAMED",
-		"--add-opens",
-		"java.base/java.lang=ALL-UNNAMED",
-
-		"-jar",
-		vim.fn.glob("~/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-		"-configuration",
-		"~/.local/share/nvim/mason/packages/jdtls/config_linux", -- Ajuste para seu SO (linux, mac, win)
-		"-data",
-		"~/.cache/jdtls-workspace", -- Diretório de workspace
-	},
-
-	-- Configurações específicas para Spring Boot
-	settings = {
-		java = {
-			configuration = {
-				updateBuildConfiguration = "automatic",
-			},
-			maven = {
-				downloadSources = true,
-			},
-			implementationsCodeLens = {
-				enabled = true,
-			},
-			referencesCodeLens = {
-				enabled = true,
-			},
-			format = {
-				enabled = true,
-			},
-			signatureHelp = {
-				enabled = true,
-			},
-			contentProvider = {
-				preferred = "fernflower",
-			},
-			import = {
-				maven = {
-					enabled = true,
-				},
-				gradle = {
-					enabled = true,
-				},
-			},
-			completion = {
-				favoriteStaticMembers = {
-					"org.junit.Assert.*",
-					"org.junit.Assume.*",
-					"org.junit.jupiter.api.Assertions.*",
-					"org.junit.jupiter.api.Assumptions.*",
-					"org.junit.jupiter.api.DynamicContainer.*",
-					"org.junit.jupiter.api.DynamicTest.*",
-					"org.mockito.Mockito.*",
-					"org.mockito.ArgumentMatchers.*",
-					"org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*",
-					"org.springframework.test.web.servlet.result.MockMvcResultMatchers.*",
-				},
-				importOrder = {
-					"java",
-					"javax",
-					"com",
-					"org",
-				},
-			},
-		},
-	},
 })
