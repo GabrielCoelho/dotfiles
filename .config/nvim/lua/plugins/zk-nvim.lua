5bab413892e83e91bff7339574493f9493714046
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2025-05-19 16:02:08 -0300

    latest configs

--- .config/nvim/lua/plugins/zk-nvim.lua
+++ .config/nvim/lua/plugins/zk-nvim.lua
@@ -2,7 +2,25 @@ return {
 	"zk-org/zk-nvim",
 	config = function()
 		require("zk").setup({
-			-- See Setup section below
+			-- Can be "telescope", "fzf", "fzf_lua", "minipick", "snacks_picker",
+			-- or select" (`vim.ui.select`).
+			picker = "telescope",
+
+			lsp = {
+				-- `config` is passed to `vim.lsp.start(config)`
+				config = {
+					cmd = { "zk", "lsp" },
+					name = "zk",
+					-- on_attach = ...
+					-- etc, see `:h vim.lsp.start()`
+				},
+
+				-- automatically attach buffers in a zk notebook that match the given filetypes
+				auto_attach = {
+					enabled = true,
+					filetypes = { "markdown" },
+				},
+			},
 		})
 	end,
 }
